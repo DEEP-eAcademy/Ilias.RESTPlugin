ebfeac411ac847b99c648afabfca7e4eec0ebb95
DEEP-eAcademy <jakub@deepeacademy.com>
2025-12-17 10:57:44 +0000

    fix: update PHP version requirements and plugin version

--- plugin.php
+++ plugin.php
@@ -10,10 +10,10 @@
 
 
 $id = "rest";
-$version = "4.0.0";
+$version = "5.0.0";
 
-$ilias_min_version = "8.0.0";
-$ilias_max_version = "8.99";
+$ilias_min_version = "9.0.0";
+$ilias_max_version = "9.999";
 
 $responsible = "Jakub";
 $responsible_mail = "jakub@deepeacademy.com";
